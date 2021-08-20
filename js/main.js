3b702c3550140bda099929101d2796056ddfa409
luisEnciso20 <luis.enciso.20@live.com>
2021-08-20 06:50:09 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -3,26 +3,46 @@
 //plantaunoFot1.style.animationFillMode = 'forwards';
 window.addEventListener('scroll',function () {
 
-    //const main = document.querySelector('#mainconta');
+
+    //////////////////1
+    const sec1 = this.document.getElementById('sec1');
+    const Planta1Ft1 = this.document.getElementById('Planta1Ft1');
+    const Planta2Ft1 = this.document.getElementById('Planta2Ft1');
+    var to1= ((window.innerHeight - 480) / 2) - 30;
+    if((sec1.getBoundingClientRect().top < (to1 * 2)) && (sec1.getBoundingClientRect().top > (to1 - 20))){
+        Planta1Ft1.style.transform ="scale(2)";
+        Planta1Ft1.style.transition = "2s";
+        Planta1Ft1.style.top = "46%";
+        Planta2Ft1.style.transform ="scale(2)";
+        Planta2Ft1.style.transition = "2s";
+        Planta2Ft1.style.top = "60%";
+    }else{
+        Planta1Ft1.style.transform ="scale(1)";
+        Planta1Ft1.style.top = "50%";
+        Planta2Ft1.style.transform ="scale(1)";
+        Planta2Ft1.style.top = "64.5%";
+    }
+
+
+    //////////////////2
     const section2 = this.document.getElementById('sec2');
     const plantaFt2 = this.document.getElementById('plantaFt2');
-
-    var to1= ((window.innerHeight - 440) / 2) - 30;
+    var to2= ((window.innerHeight - 440) / 2) - 30;
     console.log('height')
     console.log(window.innerHeight);
     console.log('top');
     console.log(section2.getBoundingClientRect().top);
     console.log('if1');
-    console.log(to1 * 2);
+    console.log(to2 * 2);
     console.log('if2');
-    console.log(to1 - 20);
+    console.log(to2 - 20);
 
-    if((section2.getBoundingClientRect().top < (to1 * 2)) && (section2.getBoundingClientRect().top > (to1 - 20))){
+    if((section2.getBoundingClientRect().top < (to2 * 2)) && (section2.getBoundingClientRect().top > (to2 - 20))){
         plantaFt2.style.transform ="scale(2)";
         //plantaFt2.style.transform = "rotate(50deg)";
         plantaFt2.style.transition = "2s";
         plantaFt2.style.top = "65.5%";
-        plantaFt2.style.left = "19.5%";
+        plantaFt2.style.left = "20.5%";
         //section2.style.backgroundColor = "blue";
     }
     else{
